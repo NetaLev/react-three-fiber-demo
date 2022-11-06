c2b3d60762efa55dca1de8f25fe0259cf0a6aaf1
Neta Lev <neta.lev@forescout.com>
2022-11-06 22:55:05 +0200

    creating a mesh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
 function App() {
-  return null;
+  return (
+    /** React-three-fiber Canvas contains THREE.js Renderer, Scene and Camera logic within it,
+     *  with default values that make sense */
+    <Canvas>
+      <mesh>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
